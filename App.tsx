110e526979252b87dbb10366525db7f989363b06
Sean <sean.lorman@gmail.com>
2020-10-21 11:57:17 -0600

    Add AppNavigator and a new home page where all the Microservices will be shown

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Routes } from "./src/Routes";
+import Examples from "./src/Examples";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator<Routes>();
+const AppNavigator = () => (
+<>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Examples"
+        component={Examples}
+        options={{
+          title: "Learn Everyday",
+        }}
+      />
+      {/* <Stack.Screen
+        name="Worklets"
+        component={Worklets}
+        options={{
+          title: "Worklets",
+        }}
+      /> */}
+    </Stack.Navigator>
+  </>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+);
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
